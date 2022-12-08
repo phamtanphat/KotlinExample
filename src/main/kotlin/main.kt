da2a631740e1ad5ec8b42d6695a2ddfe9b246743
phamtanphat <phatdroid94@gmail.com>
2022-12-08 21:13:46 +0700

    update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -17,13 +17,32 @@ fun main() {
 //    2: Toán tử -> + - x /
 //    Toán hạng: chính là các số (giá trị)
 //    Biểu thức: => đoạn xử lý mà có giá trị trả về
-    var number1: Int = 5
-    var number2: Int = 10
-    // Cộng
-    // var total: Int = number1 + number2
-    // Chia: 5 / 10 = 0
-    // phương thức chuyển đổi kiểu dữ liệu: toFloat, toString..
-    var total = number1.toFloat() / number2.toFloat()
-    print(total)
+//    var number1: Int = 5
+//    var number2: Int = 10
+//     Cộng
+//     var total: Int = number1 + number2
+//     Chia: 5 / 10 = 0
+//     phương thức chuyển đổi kiểu dữ liệu: toFloat, toString..
+//    var total = number1.toFloat() / number2.toFloat()
+//    print(total)
 
+// Bài tập: Siêu thị bán đồ giảm giá, tổng bill sẽ được giảm 20%
+//    var totalPrice = 1_051_000
+    // solution -> đưa ra trước
+//    var total = totalPrice * 0.8
+
+// Bài tập: Tính bmi
+//    Chỉ số cơ thể: cân năng / chiều cao x chiều cao
+//    var number = 5
+//    number = number + 1
+//    number += 1
+
+    // Trường hợp value++
+    // Có 2 biểu thức
+    // 1 -> Gán giá trị trước
+    // 2 -> Tăng giá trị lên 1
+    var number = 5
+    var number2 = 10
+
+    var total = number++ - --number2 + --number + number2++
 }
