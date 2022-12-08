55346c8a76d44d5f55546365b3601cab60324924
phamtanphat <phatdroid94@gmail.com>
2022-12-08 20:23:34 +0700

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // 1: Kiểu dữ liệu
+    // Kiểu số:
+    // var : Từ khoá khai báo
+    // number : Tên của biến
+    // phép gán
+    // Int -> số nguyên số không có phần thập phân
+    var number: Int = 10
+
+}
