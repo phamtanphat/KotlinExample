4718e332615360ecdfc801a3fad731d1ae40e6a6
phamtanphat <phatdroid94@gmail.com>
2022-12-08 20:53:42 +0700

    xử lý chuyên đôi kiểu dữ liệu

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,29 @@
 fun main() {
-    // 1: Kiểu dữ liệu
-    // Kiểu số:
-    // var : Từ khoá khai báo
-    // number : Tên của biến
-    // phép gán
-    // Int -> số nguyên số không có phần thập phân
-    // comment code -> Chương trình không chạy qua
-    var number: Int = 10
-    var number1: Float = 10.1f
-    var number2: Number = 10
-    var name: String = "Pham Tan phat"
-    var isMale: Boolean = true
+//     1: Kiểu dữ liệu
+//     Kiểu số:
+//     var : Từ khoá khai báo
+//     number : Tên của biến
+//     phép gán
+//     Int -> số nguyên số không có phần thập phân
+//     comment code -> Chương trình không chạy qua
+//    var number: Int = 10
+//    var number1: Float = 10.1f
+//    var number2: Number = 10
+//    var name: String = "Pham Tan phat"
+//    var isMale: Boolean = true
+
+//     Khai báo thông tin
+//     tên, địa chỉ, sở thích
+//    2: Toán tử -> + - x /
+//    Toán hạng: chính là các số (giá trị)
+//    Biểu thức: => đoạn xử lý mà có giá trị trả về
+    var number1: Int = 5
+    var number2: Int = 10
+    // Cộng
+    // var total: Int = number1 + number2
+    // Chia: 5 / 10 = 0
+    // phương thức chuyển đổi kiểu dữ liệu: toFloat, toString..
+    var total = number1.toFloat() / number2.toFloat()
+    print(total)
 
 }
