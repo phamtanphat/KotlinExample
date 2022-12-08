43c95f1aa8513bc349a8cb91fd96cb4f764bf40a
phamtanphat <phatdroid94@gmail.com>
2022-12-08 20:30:01 +0700

    type

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,6 +5,11 @@ fun main() {
     // number : Tên của biến
     // phép gán
     // Int -> số nguyên số không có phần thập phân
+    // comment code -> Chương trình không chạy qua
     var number: Int = 10
+    var number1: Float = 10.1f
+    var number2: Number = 10
+    var name: String = "Pham Tan phat"
+    var isMale: Boolean = true
 
 }
